f76200bb8fbbfe774117b8aee2d742ff2013d1f1
muahchee <187490843+muahchee@users.noreply.github.com>
2024-12-13 14:06:42 +1300

    Make operations work, new operations start fresh

--- script.js
+++ script.js
@@ -62,7 +62,12 @@ const digitButtons = document.querySelectorAll(".digit");
 
 digitButtons.forEach((digit) => {
     digit.addEventListener("click", () => {
-        //todo! 
+
+        //clears previous completed operations
+        if (equationArr.length === 1){
+            equationArr = [];
+        }
+        
         //push digit.id to inputNumber array
         //mash inputNumber into string and put in displayNumber
         inputNumber.push(digit.id);
@@ -97,5 +102,50 @@ operatorButtons.forEach((operator) => {
         }
  
     })
+})
+
+//-equals button-
+const equalsButton = document.querySelector(".equals");
+
+equalsButton.addEventListener("click", () => {
+    
+    equationArr.push(displayNumber);
+
+    //if equationArr.length === 1, return equationArr[0] in display.textContent
+    if (equationArr.length === 1){
+
+        display.textContent = equationArr[0];
+
+    } else {
+
+        //clear display and variables
+        inputNumber = [];
+        displayNumber = "";
+        display.textContent = "";     
+        
+        for (let i = 0; i < equationArr.length; i++){
+            let result = operate(Number(equationArr[0]), equationArr[1], Number(equationArr[2]));
+
+            //remove first three items and replace with result
+            equationArr.splice(0, 3, result);
+
+        }
+
+        display.textContent = equationArr[0];
+
+    }
+
+    //else if equationArr.length % 3 === 0,
+
+    //push displayNumber to equationArr
+    //clear display and variables
+
+    //loop (i = 0, i <= equationArr.length,)
+    //call operate on first three indexes, turn first and third index into numbers (firstNumber and secondNumber) and store result in 'result'
+    //remove first three items from equationArr
+    //use unshift to put result at equationArr[0]
+
+    //after loop (there should be one item in equationArr),
+    //display.textContent = equationArr[0];
 })
 
